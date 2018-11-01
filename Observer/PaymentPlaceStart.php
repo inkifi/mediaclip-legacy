bd31233d0fc779810a1f16bd071646da88e27e76
Dmitry Fedyuk <dfediuk@gmail.com>
2018-11-01 17:38:48 +0600

    CheckoutSubmitAllAfter => PaymentPlaceStart

--- Observer/PaymentPlaceStart.php
+++ Observer/PaymentPlaceStart.php
@@ -4,14 +4,13 @@ use Magento\Customer\Model\Session;
 use Magento\Framework\App\ObjectManager as OM;
 use Magento\Framework\Event\Observer;
 use Magento\Framework\Event\ObserverInterface;
-use Magento\Sales\Model\Order as O;
 use Magento\Sales\Model\Order\Payment as OP;
 /**
  * 2018-06-27 Dmitry Fedyuk https://www.upwork.com/fl/mage2pro
  * "Monitor orders and pass the «Payment Complete» ones to MediaClip Photobook in my Magento 2 store":
  * https://www.upwork.com/ab/f/contracts/20295611
  */
-final class CheckoutSubmitAllAfter implements ObserverInterface {
+final class PaymentPlaceStart implements ObserverInterface {
 	/**
 	 * 2018-06-27
 	 * @override
@@ -20,13 +19,9 @@ final class CheckoutSubmitAllAfter implements ObserverInterface {
 	 * @param Observer $ob
 	 */
 	function execute(Observer $ob) {
-		$o = $ob['order']; /** @var O $o */
-		if ('pending_payment' === $o->getStatus()) {
-			$om = OM::getInstance(); /** @var OM $om */
-			$session = $om->create(Session::class);
-			$op = $o->getPayment(); /** @var OP $op */
-			$op->setAdditionalInformation('df_mediaclip_customer_id', $session->getMediaClipUserId());
-			$op->save();
-		}
+		$op = $ob['payment']; /** @var OP $op */
+		$om = OM::getInstance(); /** @var OM $om */
+		$session = $om->create(Session::class);
+		$op->setAdditionalInformation('df_mediaclip_customer_id', $session->getMediaClipUserId());
 	}
 }
