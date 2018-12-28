aec51280cc4f5446b66b2b3a77ad9f86c59d74ba
Dmitry Fedyuk <dfediuk@gmail.com>
2018-12-28 11:36:36 +0700

    no message

--- Controller/Index/OneflowResponse.php
+++ Controller/Index/OneflowResponse.php
@@ -19,7 +19,26 @@ class OneflowResponse extends \Magento\Framework\App\Action\Action {
 		$logger->addWriter($writer);
 		$json = file_get_contents('php://input');
 		$logger->info($json);
-		/*$json = '{"TimeStamp": "2018-04-06T10:20:50.506Z","SourceOrderId": "39477","SourceShipmentId": "","ShipmentIndex": "0","TrackingNumber": "","OrderStatus": "shipped"}';*/
+		/**
+		 * 2018-12-28 Dmitry Fedyuk https://www.upwork.com/fl/mage2pro
+		 * "Improve MediaClip module for Magento 2: handle shipping notifications for the US store":
+		 * https://www.upwork.com/ab/f/contracts/21253740
+		 * https://www.upwork.com/messages/rooms/room_33abff92763bac6bac92a394ada0f09c/story_54a27db7705e1c75e5964af8bad331dc
+		 * https://www.upwork.com/messages/rooms/room_8f56da8c65d755371bad23ddebe0721a
+		 * A request for the US store is invalid, e.g.:
+		 * {
+		 *		"TimeStamp": "2018-12-08T05:52:56.584Z",
+		 *		"SourceOrderId": "53297",
+		 *		"SourceItemId": "",
+		 *		"SourceShipmentId": "",
+		 *		"ShipmentIndex": "0",
+		 *		"TrackingNumber": "9405515901453214355474",
+		 *		"Carrier": shqcustomc_po_pm
+		 *		"OrderStatus": "Shipped"
+		 *	}
+		 * I make it valid.
+		 */
+		$json = preg_replace('#"Carrier":\s*(\w+)#', '"Carrier": "$1",', $json);
 		$req = json_decode($json, true);
 		if (!empty($req) && isset($req['OrderStatus']) && 'shipped' === $req['OrderStatus']) {
 			$oid = intval($req['SourceOrderId']); /** @var int $oid */
@@ -31,7 +50,7 @@ class OneflowResponse extends \Magento\Framework\App\Action\Action {
 			$oc = df_new_om(OC::class); /** @var OC $oc */
 			$shipment = $oc->toShipment($o); /** @var Shipment $shipment */
 			foreach ($o->getAllItems() AS $oi) { /** @var OI $oi */
-				if (! $oi->getQtyToShip() || $oi->getIsVirtual()) {
+				if (!$oi->getQtyToShip() || $oi->getIsVirtual()) {
 					continue;
 				}
 				$qtyShipped = $oi->getQtyToShip();
