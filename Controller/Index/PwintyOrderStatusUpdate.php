bf12bb5f9fb498a140905d784c53290b06ec0749
Dmitry Fedyuk <dfediuk@gmail.com>
2019-02-23 23:05:29 +0700

    https://github.com/Inkifi-Connect/Media-Clip-Inkifi/issues/10

--- Controller/Index/PwintyOrderStatusUpdate.php
+++ Controller/Index/PwintyOrderStatusUpdate.php
@@ -1,16 +1,12 @@
 <?php 
-
-
 namespace Mangoit\MediaclipHub\Controller\Index;
- 
 use Magento\Framework\App\Action\Action;
 use Magento\Framework\App\Action\Context;
+use Magento\Framework\Exception\LocalizedException as LE;
 use Magento\Framework\View\Result\PageFactory;
 use pwinty\PhpPwinty;
-
-class PwintyOrderStatusUpdate extends Action
-{
-        /**
+class PwintyOrderStatusUpdate extends Action {
+	/**
      * @var Magento\Framework\View\Result\PageFactory
      */
     protected $_resultPageFactory;
@@ -23,17 +19,13 @@ class PwintyOrderStatusUpdate extends Action
         Context $context,
         \Magento\Sales\Model\Order\Shipment\TrackFactory $trackFactory,
         PageFactory $resultPageFactory
- 
     ) {
         $this->_resultPageFactory = $resultPageFactory;
         $this->trackFactory = $trackFactory;
         parent::__construct($context);
- 
     }
  
-    function execute()
-    {
-        
+    function execute() {
         /*$writer = new \Zend\Log\Writer\Stream(BP . '/var/log/pwinty_orders_status.log');
         $logger = new \Zend\Log\Logger();
         $logger->addWriter($writer);
@@ -44,27 +36,25 @@ class PwintyOrderStatusUpdate extends Action
         //$logger->info('Array Log'.print_r($myArrayVar, true));
         //Mage::log($json, null, 'mediaclip_orders_download_shipment_status.log');
     $json ='{"id":971961,"status":"Complete","shipments":[{"status":"shipped","items":[6468905],"trackingNumber":"MQ121286142GB","trackingUrl":"http://www.royalmail.com/portal/rm/track?trackNumber=MQ121286142GB"}],"environment":"live","timestamp":"2017-12-11T17:39:00.2368387Z"}';
-        $obj = json_decode($json, true);
-       echo "<pre>"; print_r($obj); 
+		$obj = json_decode($json, true);
+		echo "<pre>"; print_r($obj);
         if (isset($obj['shipments'])) {
             foreach ($obj['shipments'] as $value) {
                 if ($value['status'] == 'shipped') {
-                   
                     $pwintyorderId = $obj['id'] ;
-                    $mediaclipOrderModel = $this->_objectManager->create('Mangoit\MediaclipHub\Model\Orders');
+                    $mediaclipOrderModel = df_new_om('Mangoit\MediaclipHub\Model\Orders');
                     $mediaclipOrderModelCollection = $mediaclipOrderModel->getCollection();
                     $mediaclipOrder = $mediaclipOrderModelCollection->addFieldToFilter('pwinty_order_id', array('eq' => '682012'));
                     //print_r($mediaclipOrder->getData()[0]['magento_order_id']);
-                    $order = $this->_objectManager->create('Magento\Sales\Model\Order')->load(
+                    $order = df_new_om('Magento\Sales\Model\Order')->load(
 						// 2018-08-16 Dmitry Fedyuk https://www.upwork.com/fl/mage2pro
 						// «Modify orders numeration for Mediaclip»
 						// https://github.com/Inkifi-Connect/Media-Clip-Inkifi/issues/1
 						ikf_eti($mediaclipOrder->getData()[0]['magento_order_id'])
 					);
                     // Check if order can be shipped or has already shipped
-
                     if (! $order->canShip()) {
-                        throw new \Magento\Framework\Exception\LocalizedException( __('You can\'t create an shipment.'));
+                        throw new LE(__('You can\'t create an shipment.'));
                     }
 
                     foreach ($mediaclipOrder as $key => $trackingValue) {
@@ -74,7 +64,7 @@ class PwintyOrderStatusUpdate extends Action
                         $trackingValue->save();
                     }
                     // Initialize the order shipment object
-                    $convertOrder = $this->_objectManager->create('Magento\Sales\Model\Convert\Order');
+                    $convertOrder = df_new_om('Magento\Sales\Model\Convert\Order');
                     $shipment = $convertOrder->toShipment($order);
 
                     // Loop through order items
@@ -111,12 +101,12 @@ class PwintyOrderStatusUpdate extends Action
                         $shipment->save();
                         $shipment->getOrder()->save();
                         // Send email
-                        $this->_objectManager->create('Magento\Shipping\Model\ShipmentNotifier')
+                        df_new_om('Magento\Shipping\Model\ShipmentNotifier')
                             ->notify($shipment);
 
                         $shipment->save();
                     } catch (\Exception $e) {
-                        throw new \Magento\Framework\Exception\LocalizedException(
+                        throw new LE(
                                         __($e->getMessage())
                                     );
                     }
@@ -129,26 +119,26 @@ class PwintyOrderStatusUpdate extends Action
     /*function FunctionName($value='')
     {
         // Load the order increment ID
-        $order = $this->_objectManager->create('Magento\Sales\Model\Order')->loadByIncrementID($incrementid);
+        $order = df_new_om('Magento\Sales\Model\Order')->loadByIncrementID($incrementid);
 
         // OR
-        $order = $this->_objectManager->create('Magento\Sales\Model\Order')
+        $order = df_new_om('Magento\Sales\Model\Order')
             ->loadByAttribute('increment_id', '000000001');
 
 
         //load by order 
-        $order = $this->_objectManager->create('Magento\Sales\Model\Order')
+        $order = df_new_om('Magento\Sales\Model\Order')
             ->load('1');
 
         // Check if order can be shipped or has already shipped
         if (! $order->canShip()) {
-            throw new \Magento\Framework\Exception\LocalizedException(
+            throw new LE(
                             __('You can\'t create an shipment.')
                         );
         }
 
         // Initialize the order shipment object
-        $convertOrder = $this->_objectManager->create('Magento\Sales\Model\Convert\Order');
+        $convertOrder = df_new_om('Magento\Sales\Model\Convert\Order');
         $shipment = $convertOrder->toShipment($order);
 
         // Loop through order items
@@ -178,12 +168,12 @@ class PwintyOrderStatusUpdate extends Action
             $shipment->getOrder()->save();
 
             // Send email
-            $this->_objectManager->create('Magento\Shipping\Model\ShipmentNotifier')
+            df_new_om('Magento\Shipping\Model\ShipmentNotifier')
                 ->notify($shipment);
 
             $shipment->save();
         } catch (\Exception $e) {
-            throw new \Magento\Framework\Exception\LocalizedException(
+            throw new LE(
                             __($e->getMessage())
                         );
         }
