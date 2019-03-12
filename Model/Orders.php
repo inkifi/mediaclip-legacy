eed34617a4ac6187f9451ec7c45044484ac5550c
Dmitry Fedyuk <dfediuk@gmail.com>
2019-03-13 05:13:15 +0630

    no message

--- Model/Orders.php
+++ Model/Orders.php
@@ -1,6 +1,7 @@
 <?php
 namespace Mangoit\MediaclipHub\Model;
 use Mangoit\MediaclipHub\Model\ResourceModel\Orders as R;
+use Mangoit\MediaclipHub\Model\ResourceModel\Orders\Collection as C;
 /**
  * 2019-03-13
  * A `mediaclip_orders` row is associated with an unique `magento_order_id`,
@@ -18,4 +19,33 @@ use Mangoit\MediaclipHub\Model\ResourceModel\Orders as R;
 class Orders extends \Magento\Framework\Model\AbstractModel {
 	/** 2019-03-13 */
 	function _construct() {$this->_init(R::class);}
+
+	/**
+	 * 2019-03-13
+	 * @used-by \Inkifi\Mediaclip\H\AvailableForDownload::_p()
+	 */
+	function markAsDownloaded() {
+		$this->setData(self::F__DOWNLOAD_STATUS, 1);
+		$this->save();
+	}
+
+	/**
+	 * 2019-03-13
+	 * @used-by \Inkifi\Mediaclip\Event::mo()
+	 * @param string $oidE		«58312» or «staging-58312»
+	 * @return self
+	 */
+	static function byOIdE($oidE) {
+		$c = C::i();
+		$c->addFieldToFilter('magento_order_id', ['eq' => $oidE]);
+		df_assert_eq(1, $c->count());
+		return $c->getFirstItem();
+	}
+
+	/**
+	 * 2019-03-13
+	 * @used-by markAsDownloaded()
+	 * @used-by \Mangoit\MediaclipHub\Setup\UpgradeSchema::upgrade()
+	 */
+	const F__DOWNLOAD_STATUS = 'order_download_status';
 }
--- Model/ResourceModel/Orders/Collection.php
+++ Model/ResourceModel/Orders/Collection.php
@@ -6,4 +6,11 @@ use Mangoit\MediaclipHub\Model\ResourceModel\Orders as R;
 class Collection extends \Magento\Framework\Model\ResourceModel\Db\Collection\AbstractCollection {
 	/** 2019-03-13 */
 	function _construct() {$this->_init(M::class, R::class);}
+
+	/**
+	 * 2019-03-13
+	 * @used-by \Mangoit\MediaclipHub\Model\Orders::byOIdE()
+	 * @return self
+	 */
+	static function i() {return df_new_om(__CLASS__);}
 }
