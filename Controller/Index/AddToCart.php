28cf41ca65d7401bef7f6b62621abe2f5e76adbf
Dmitry Fedyuk <dfediuk@gmail.com>
2019-05-15 08:22:23 +0600

    no message

--- Controller/Index/AddToCart.php
+++ Controller/Index/AddToCart.php
@@ -1,5 +1,9 @@
 <?php
 namespace Mangoit\MediaclipHub\Controller\Index;
+use Inkifi\Mediaclip\Price;
+use Magento\Catalog\Model\Product as P;
+use Magento\Quote\Model\Quote\Item as QI;
+use Magento\Quote\Model\Quote\Item\Option as QIO;
 // 2019-04-17
 class AddToCart extends \Magento\Framework\App\Action\Action {
 	/**
@@ -7,190 +11,191 @@ class AddToCart extends \Magento\Framework\App\Action\Action {
 	 * A request: `/mediacliphub/index/addToCart?projectId=c9264194-eace-45a1-b951-e849db3842a8`
 	 */
 	function execute() {
-		$projectId = $this->getRequest()->getParam('projectId');
-		$quote = df_cart()->getQuote();
-		$addToCart = true;
-		foreach ($quote->getAllVisibleItems() as $item) {
-			if ($item->getMediaclipProjectId()) {
-				if ($item->getMediaclipProjectId() == $projectId) {
-				 //echo "<pre>";  print_r($item->getData());
-					$quote_item = $this->_objectManager->create('\Magento\Quote\Model\Quote\Item\Option')->load($item->getItemId(),'item_id');
-
-					$product_id = $quote_item->getProductId();
-
-					$product = $this->_objectManager->create('\Magento\Catalog\Model\Product')->load($product_id);
-
-					$mediaClipProject = $this->getMediaClipProject($projectId);
-
-					$obj = json_decode($mediaClipProject->getProjectDetails(), true);
-
-					$quantity = 1;
-
-					$checkToAppendQty = $this->checkToAppendQty($product);
-					if ($checkToAppendQty) {
-						$quantity = $this->getProductQuantity($obj['items']);
-					}
-
-					$price = $this->getProductCalculatedPrice($product_id, $obj, 1);
-
-
-					$productOptions = $product->getOptions();
-					$product_option = array();
-					if ($productOptions) {
-						foreach($productOptions as $optionId => $optionDetail){
-
-							if ($optionDetail->getDefaultTitle() == 'Additional') {
-								$additional = $this->getAdditionalSheetsPhotobook($mediaClipProject, $product_id);
-								$product_option[$optionId] = $additional;
-								$product_option['option_'.$optionId] = $additional;
-							}
+		try {
+			$projectId = $this->getRequest()->getParam('projectId');
+			$quote = df_cart()->getQuote();
+			$addToCart = true;
+			foreach ($quote->getAllVisibleItems() as $qi) { /** @var QI $qi */
+				if ($qi->getMediaclipProjectId()) {
+					if ($qi->getMediaclipProjectId() == $projectId) {
+						/** @var QIO $qio */
+						$qio = df_new_om(QIO::class)->load($qi->getItemId(),'item_id');
+						$pid = (int)$qio->getProductId(); /** @var int $pid */
+						/**
+						 * 2019-05-15
+						 * 1) «Make the Mediaclip's «Get Price» endpoint compatible
+						 * with the Magento 2 multistore mode»:
+						 * https://github.com/Inkifi-Connect/Media-Clip-Inkifi/issues/13
+						 * 2) https://magento.stackexchange.com/a/177164
+						 */
+						$product = df_product($pid, true); /** @var P $product */
+						$mediaClipProject = $this->getMediaClipProject($projectId);
+						$obj = json_decode($mediaClipProject->getProjectDetails(), true);
+						$quantity = 1;
+						$checkToAppendQty = $this->checkToAppendQty($product);
+						if ($checkToAppendQty) {
+							$quantity = $this->getProductQuantity($obj['items']);
 						}
-					}
+						$price = Price::get($product, $obj, 1, true);
+						$productOptions = $product->getOptions();
+						$product_option = array();
+						if ($productOptions) {
+							foreach($productOptions as $optionId => $optionDetail){
 
-					$itemOptions = $item->getOptions();
-
-					foreach ($itemOptions as $option) {
-						//print_r($option->getData());
-						if($option->getCode() == 'info_buyRequest')
-						{
-							$optionVal = $option->getValue();
-							$unserialized = json_decode($optionVal,true);
-							//$unserialized = unserialize($optionVal);
-							//print_r($unserialized);
-							if (isset($unserialized['options'])) {
-								foreach ($unserialized['options'] as $key => $value) {
-									if (isset($product_option[$key])) {
-										$unserialized['options'][$key]= $product_option[$key];
-										$option->setValue(json_encode($unserialized));
-									}
+								if ($optionDetail->getDefaultTitle() == 'Additional') {
+									$additional = $this->getAdditionalSheetsPhotobook($mediaClipProject, $pid);
+									$product_option[$optionId] = $additional;
+									$product_option['option_'.$optionId] = $additional;
 								}
 							}
-						}elseif (isset($product_option[$option->getCode()])) {
-							$option->setValue($product_option[$option->getCode()]);
 						}
-					}
-					$storeId = df_store_id(); /** @var int $storeId */
-					$item->setStoreId($storeId);
-					$item->setOptions($itemOptions);
-					$item->setCustomPrice($price);
-					$item->setOriginalCustomPrice($price);
-					$item->getProduct()->setIsSuperMode(true);
-					$item->setQty($quantity);
-					$item->setProduct($product);
-					$item->save();
-					$quote->collectTotals()->save();
-
-					$this->messageManager->addSuccess(__('Add to cart successfully.'));
-					//$session->addSuccess($message);
-
-					$addToCart = false;
-				}
-			}
-		}
-
-		if ($addToCart) {
-
-			$mediaClipProject = $this->getMediaClipProject($projectId);
-
-
-
-			$product_id = $mediaClipProject->getStoreProductId();
-
-			$product = $this->_objectManager->create('\Magento\Catalog\Model\Product')->load($product_id);
-
-			//$formKey = $session->getFormKey();
-			$formKey = $this->_objectManager->get('Magento\Framework\Data\Form\FormKey')->getFormKey();
-			$obj = df_json_decode($mediaClipProject->getProjectDetails());
-			df_assert($obj);
-			$quantity = 1;
-			$checkToAppendQty = $this->checkToAppendQty($product);
-			if ($checkToAppendQty) {
-				$quantity = $this->getProductQuantity($obj['items']);
-			}
-			$price = $this->getProductCalculatedPrice($product_id, $obj, 1);
-
-			$options = $product->getOptions();
-			$option = array();
-			if ($options) {
-				foreach($options as $optionDetail){
-
-
-					$optionTitle = $optionDetail->getTitle();
-					if ($optionTitle == 'Additional') {
-						$optionArray[$optionDetail->getData('option_id')] = $this->getAdditionalSheetsPhotobook($mediaClipProject, $product_id);
-					} else if($optionTitle == 'Project') {
-						$optionArray[$optionDetail->getData('option_id')] = $projectId;
-					} else {
-						$project_details = $mediaClipProject->getProjectDetails();
-						if ($project_details) {
-							$project_details = json_decode($project_details, true);
-							$optionsDetails = json_decode($project_details['properties']['option_details'], true);
+						$itemOptions = $qi->getOptions();
+						foreach ($itemOptions as $option) {
+							//print_r($option->getData());
+							if($option->getCode() == 'info_buyRequest')
+							{
+								$optionVal = $option->getValue();
+								$unserialized = json_decode($optionVal,true);
+								//$unserialized = unserialize($optionVal);
+								//print_r($unserialized);
+								if (isset($unserialized['options'])) {
+									foreach ($unserialized['options'] as $key => $value) {
+										if (isset($product_option[$key])) {
+											$unserialized['options'][$key]= $product_option[$key];
+											$option->setValue(json_encode($unserialized));
+										}
+									}
+								}
+							}elseif (isset($product_option[$option->getCode()])) {
+								$option->setValue($product_option[$option->getCode()]);
+							}
 						}
-						$optionArray[$optionDetail->getData('option_id')] = $optionsDetails[$optionDetail->getData('option_id')];
+						$storeId = df_store_id(); /** @var int $storeId */
+						$qi->setStoreId($storeId);
+						$qi->setOptions($itemOptions);
+						$qi->setCustomPrice($price);
+						$qi->setOriginalCustomPrice($price);
+						$qi->getProduct()->setIsSuperMode(true);
+						$qi->setQty($quantity);
+						$qi->setProduct($product);
+						$qi->save();
+						$quote->collectTotals()->save();
+
+						$this->messageManager->addSuccess(__('Add to cart successfully.'));
+						//$session->addSuccess($message);
+
+						$addToCart = false;
 					}
 				}
-				$option['options'] = $optionArray;
 			}
-			$params = array(
-				'product' => $product_id,
-				'form_key' => $formKey,
-				'qty' => $quantity,
-				'price' => $price,
-			);
+			if ($addToCart) {
+				$mediaClipProject = $this->getMediaClipProject($projectId);
+				$pid = $mediaClipProject->getStoreProductId();
+				/**
+				 * 2019-05-15
+				 * 1) «Make the Mediaclip's «Get Price» endpoint compatible
+				 * with the Magento 2 multistore mode»:
+				 * https://github.com/Inkifi-Connect/Media-Clip-Inkifi/issues/13
+				 * 2) https://magento.stackexchange.com/a/177164
+				 */
+				$product = df_product($pid, true); /** @var P $product */
+				//$formKey = $session->getFormKey();
+				$formKey = $this->_objectManager->get('Magento\Framework\Data\Form\FormKey')->getFormKey();
+				$obj = df_json_decode($mediaClipProject->getProjectDetails());
+				df_assert($obj);
+				$quantity = 1;
+				$checkToAppendQty = $this->checkToAppendQty($product);
+				if ($checkToAppendQty) {
+					$quantity = $this->getProductQuantity($obj['items']);
+				}
+				$price = Price::get($product, $obj, 1, true);
 
-			if (!empty($option)) {
-				$params = array_merge($params, $option);
-			}
-			try{
-
-					$product = $this->_objectManager->create('Magento\Catalog\Model\Product')->load($product_id);
+				$options = $product->getOptions();
+				$option = array();
+				if ($options) {
+					foreach($options as $optionDetail){
+						$optionTitle = $optionDetail->getTitle();
+						if ($optionTitle == 'Additional') {
+							$optionArray[$optionDetail->getData('option_id')] = $this->getAdditionalSheetsPhotobook($mediaClipProject, $pid);
+						} else if($optionTitle == 'Project') {
+							$optionArray[$optionDetail->getData('option_id')] = $projectId;
+						} else {
+							$project_details = $mediaClipProject->getProjectDetails();
+							if ($project_details) {
+								$project_details = json_decode($project_details, true);
+								$optionsDetails = json_decode(
+									$project_details['properties']['option_details'], true
+								);
+							}
+							$optionArray[$optionDetail->getData('option_id')] = $optionsDetails[$optionDetail->getData('option_id')];
+						}
+					}
+					$option['options'] = $optionArray;
+				}
+				$params = array(
+					'product' => $pid,
+					'form_key' => $formKey,
+					'qty' => $quantity,
+					'price' => $price,
+				);
+				if (!empty($option)) {
+					$params = array_merge($params, $option);
+				}
+				try {
+					/**
+					 * 2019-05-15
+					 * 1) «Make the Mediaclip's «Get Price» endpoint compatible
+					 * with the Magento 2 multistore mode»:
+					 * https://github.com/Inkifi-Connect/Media-Clip-Inkifi/issues/13
+					 * 2) https://magento.stackexchange.com/a/177164
+					 */
+					$product = df_product($pid, true); /** @var P $product */
 					df_cart()->addProduct($product, $params)->save();
-					 $this->messageManager->addSuccess(__('Add to cart successfully.'));
-
-				foreach ($quote->getAllVisibleItems() as $item) {
-
-					if ($item->getMediaclipProjectId()) {
-						if ($item->getMediaclipProjectId() == $projectId) {
-
-							$quote_item = $this->_objectManager->create('\Magento\Quote\Model\Quote\Item\Option')->load($item->getId());
-							//echo "<pre>"; print_r($item->getData()); die();
-							$product_id = $quote_item->getProductId();
-
-							$product = $this->_objectManager->create('\Magento\Catalog\Model\Product')->load($product_id);
-
-							$item->setCustomPrice($price);
-							$item->setOriginalCustomPrice($price);
-							$item->getProduct()->setIsSuperMode(true);
-							//$item->setProduct($product);
-							$item->save();
+					$this->messageManager->addSuccess(__('Add to cart successfully.'));
+					foreach ($quote->getAllVisibleItems() as $qi) { /** @var QI $qi */
+						if ($qi->getMediaclipProjectId()) {
+							if ($qi->getMediaclipProjectId() == $projectId) {
+								$qi->setCustomPrice($price);
+								$qi->setOriginalCustomPrice($price);
+								$qi->getProduct()->setIsSuperMode(true);
+								$qi->save();
+							}
 						}
 					}
-
 				}
-			   //echo "<pre>"; print_r(get_class_methods($quote));die();
-				//$session->addSuccess($message);
+				catch (\Magento\Framework\Exception\LocalizedException $e) {
+					 $this->messageManager->addException($e, __('%1', $e->getMessage()));
+					 df_log($e, $this);
+				}
+				catch (\Exception $e) {df_log($e, $this);}
+				$quote->collectTotals()->save();
 			}
-			catch (\Magento\Framework\Exception\LocalizedException $e) {
-				 $this->messageManager->addException(
-					 $e,
-					 __('%1', $e->getMessage())
-				 );
-			}
-			catch (\Exception $e) {}
-			$quote->collectTotals()->save();
 		}
+		catch (\Exception $e) {df_log($e, $this);}
 		// 2019-05-08 Dmitry Fedyuk https://www.upwork.com/fl/mage2pro
 		// «Fix the Mediaclip edit cart scenario for the US store»
 		// https://github.com/Inkifi-Connect/Media-Clip-Inkifi/issues/12
 		$this->getResponse()->setRedirect(df_url('checkout/cart/index'));
 	}
 
-	function getMediaClipProject($projectId){
+	/**
+	 * 2019-05-15
+	 * @used-by execute()
+	 * @param $projectId
+	 * @return mixed
+	 */
+	private function getMediaClipProject($projectId){
 		return $this->_objectManager->get('\Mangoit\MediaclipHub\Model\Mediaclip')->load($projectId, 'project_id');
 	}
 
-	function checkToAppendQty($product){
 
+	/**
+	 * 2019-05-15
+	 * @used-by execute()
+	 * @param $product
+	 * @return bool
+	 */
+	private function checkToAppendQty($product){
 		$response = true;
 		if ($product->getMediaclipMinimumPrintsAllow() && $product->getMediaclipMinimumPrintsCount() != '' && $product->getMediaclipExtraPrintsPrice() != '') {
 			return false;
@@ -198,7 +203,13 @@ class AddToCart extends \Magento\Framework\App\Action\Action {
 		return $response;
 	}
 
-	function getProductQuantity($items){
+	/**
+	 * 2019-05-15
+	 * @used-by execute()
+	 * @param $items
+	 * @return int
+	 */
+	private function getProductQuantity($items){
 		$quantity = 0;
 		if (!empty($items)) {
 			foreach ($items as $item) {
@@ -206,94 +217,5 @@ class AddToCart extends \Magento\Framework\App\Action\Action {
 			}
 		}
 		return $quantity;
-	}
-
-	function getProductCalculatedPrice($product_id, $mediaclip_obj, $quantity){
-
-		$price = 0;
-
-		$product = $this->_objectManager->create('\Magento\Catalog\Model\Product');
-		$product = $product->load($product_id);
-
-		if ($product) {
-			$product_price = $product->getPrice();
-			$custom_option_price = 0;
-			$additionalPrice = 0;
-
-			if (isset($mediaclip_obj['properties']['option_details']) && !empty($mediaclip_obj['properties']['option_details'])) {
-				$option_details = json_decode($mediaclip_obj['properties']['option_details']);
-
-				$product_options = $product->getOptions();
-
-				foreach ($option_details as $oid => $ovalue) {
-					if ($product_options) {
-
-						foreach ($product_options as $pvalue) {
-							 //print_r($pvalue->getData('option_id'));
-						 //print_r($pvalue);
-							if ($oid == $pvalue->getData('option_id')) {
-								$optionValues = $pvalue->getValues();
-								if ($optionValues) {
-
-									foreach ($optionValues as $_value) {
-										if ($ovalue == $_value->getData('option_type_id')) {
-											$custom_option_price = $custom_option_price + $_value->getPrice();
-										}
-									}
-								}
-							}
-						}
-					}
-				}
-			}
-			foreach ($mediaclip_obj['items'] as $key => $value) {
-				if (isset($value['photobookData']) && isset($value['photobookData']['additionalSheetCount'])) {
-					if ($value['photobookData']['additionalSheetCount'] > 0) {
-						$additionalPriceAmount = $this->getProductAdditionalPriceAmount($product_id);
-						$additional = $value['photobookData']['additionalSheetCount'];
-						$additionalPrice = $additionalPrice + ($additionalPriceAmount * (int)$additional);
-					}
-				}
-			}
-
-			if ($product->getMediaclipMinimumPrintsAllow()) {
-				$min_allow = $product->getMediaclipMinimumPrintsCount();
-				$add_price = $product->getMediaclipExtraPrintsPrice();
-				if ($min_allow != '' && is_numeric($min_allow) && $add_price != '' && is_numeric($add_price)) {
-					foreach ($mediaclip_obj['items'] as $mediaclipItem) {
-						if ($mediaclipItem['properties']['storeProductId'] == $product_id) {
-							$designer_prints = $mediaclipItem['quantity'];
-							if ($designer_prints > $min_allow) {
-								$diff = $designer_prints - $min_allow;
-								$additionalPrice = $diff*$add_price;
-							} else if ($designer_prints < $min_allow) {
-								$diff = $min_allow - $designer_prints;
-								$session = $this->_objectManager->get('Magento\Customer\Model\Session');
-								$session->setCanAddMoreMediaclipPrintsPrompt(1);
-								$add_more_prompt['diff'] = $diff;
-								$add_more_prompt['project_id'] = $mediaclip_obj['projectId'];
-								$add_more_prompt['product_id'] = $product_id;
-								$session->setAddMoreMediaclipPrints($add_more_prompt);
-							}
-						}
-					}
-				}
-			}
-
-			$price = $product_price + $custom_option_price + $additionalPrice;
-			$price = $price*$quantity;
-		}
-		$session = $this->_objectManager->get('Magento\Customer\Model\Session');
-		$session->setCustomPriceObserver($price);
-		return $price;
-	}
-
-	function getProductAdditionalPriceAmount($productId){
-		$product = $this->_objectManager->create('\Magento\Catalog\Model\Product')->load($productId);
-		if ($product->getMediaClipExtrasheetamt() && is_numeric($product->getMediaClipExtrasheetamt())) {
-			$additionalAmount = $product->getMediaClipExtrasheetamt();
-			return $additionalAmount;
-		}
-		return 0;
 	}
 }
--- Controller/Index/GetPriceEndpoint.php
+++ Controller/Index/GetPriceEndpoint.php
@@ -1,36 +1,80 @@
 <?php
 namespace Mangoit\MediaclipHub\Controller\Index;
-use Magento\Framework\App\Action\Action;
-use Magento\Framework\App\Action\Context;
-use Magento\Framework\View\Result\PageFactory;
-class GetPriceEndpoint extends Action {
-	protected $logger;
+use Df\Framework\W\Result\Json;
+use Inkifi\Mediaclip\Price;
+use Magento\Catalog\Model\Product as P;
+/**
+ * 2019-05-15
+ * «Make the Mediaclip's «Get Price» endpoint compatible with the Magento 2 multistore mode»
+ * https://github.com/Inkifi-Connect/Media-Clip-Inkifi/issues/13
+ * https://doc.mediaclip.ca/hub/store-endpoints/get-price
+ * @final Unable to use the PHP «final» keyword here because of the M2 code generation.
+ */
+class GetPriceEndpoint extends \Df\Framework\Action {
 	/**
-	 * @param Context     $context
-	 * @param PageFactory $resultPageFactory
+	 * 2019-05-15
+	 * @final Unable to use the PHP «final» keyword here because of the M2 code generation.
+	 * @override
+	 * @see \Magento\Framework\App\Action\Action::execute()
+	 * @used-by \Magento\Framework\App\Action\Action::dispatch():
+	 * 		$result = $this->execute();
+	 * https://github.com/magento/magento2/blob/2.2.1/lib/internal/Magento/Framework/App/Action/Action.php#L84-L125
+	 * @return Json
 	 */
-	function __construct(Context $context, \Psr\Log\LoggerInterface $logger, PageFactory $resultPageFactory) {
-		$this->logger = $logger;
-		parent::__construct($context);
- 
-	}
-
-	/** 2019-05-14 */
 	function execute() {
-		$json = file_get_contents('php://input');
-		$obj = json_decode($json, true);
-		$product_id = $obj['properties']['storeProductId'];
-		$quantity = 1;
-		$product = $this->_objectManager->create('\Magento\Catalog\Model\Product')->load($product_id);
-		$checkToAppendQty = $this->checkToAppendQty($product);
-		if ($checkToAppendQty) {
-			$quantity = $this->getProductQuantity($obj['items']);
+		/** @var array(string => mixed) $r */
+		try {
+			/**
+			 * 2019-05-15
+			 * https://doc.mediaclip.ca/hub/store-endpoints/get-price
+			 * A request:
+			 * {
+			 *		"storeData": {
+			 *			"userId": "70994"
+			 *		},
+			 *		"projectId": "bcd16e0f-241f-4294-a8a0-094b709ac53e",
+			 *		"properties": {
+			 *			"storeProductId": "80321"
+			 *		},
+			 *		"items": [
+			 *			{
+			 *				"productId": "$(package:inkifi/us-prints)/products/mini-prints",
+			 *				"plu": "US-INKIFI-MP",
+			 *				"quantity": 1,
+			 *				"properties": {
+			 *					"storeProductId": "80321"
+			 *				}
+			 *			}
+			 *		]
+			 *	}
+			 */
+			$req = df_json_decode(file_get_contents('php://input')); /** @var array(string => mixed) $req */
+			df_sentry_extra($this, 'Request', $req);
+			$pid = (int)$req['properties']['storeProductId']; /** @var int $pid */
+			$quantity = 1;
+			/**
+			 * 2019-05-15
+			 * 1) «Make the Mediaclip's «Get Price» endpoint compatible with the Magento 2 multistore mode»
+			 * https://github.com/Inkifi-Connect/Media-Clip-Inkifi/issues/13
+			 * 2) https://magento.stackexchange.com/a/177164
+			 */
+			$p = df_product($pid, true); /** @var P $p */
+			$checkToAppendQty = $this->checkToAppendQty($p);
+			if ($checkToAppendQty) {
+				$quantity = $this->getProductQuantity($req['items']);
+			}
+			$price = Price::get($p, $req, $quantity);
+			$r = ['price' => ['original' => $this->getPriceHtml($price)]];
 		}
-		$price = $this->getProductCalculatedPrice($product_id, $obj, $quantity);
-		$price = $this->getPriceHtml($price);
-		$priceData = array("price" => array("original" => $price));
-		echo json_encode($priceData);
-		exit;
+		catch (\Exception $e) {
+			df_response_code(500);
+			$r = ['error' => df_ets($e)];
+			df_log($e, $this);
+			if (df_my_local()) {
+				throw $e; // 2016-03-27 It is convenient for me to the the exception on the screen.
+			}
+		}
+		return Json::i($r);
 	}
 
 	/**
@@ -61,96 +105,6 @@ class GetPriceEndpoint extends Action {
 			}
 		}
 		return $quantity;
-	}
-
-	/**
-	 * 2019-05-14
-	 * @used-by execute()
-	 * @param $product_id
-	 * @param $mediaclip_obj
-	 * @param $quantity
-	 * @return float|int
-	 */
-	private function getProductCalculatedPrice($product_id, $mediaclip_obj, $quantity){
-		$price = 0;
-		$product = $this->_objectManager->create('\Magento\Catalog\Model\Product');
-		$product->load($product_id);
-		if ($product) {
-			$product_price = $product->getPrice();
-			$custom_option_price = 0;
-			$additionalPrice = 0;
-			if (isset($mediaclip_obj['properties']['option_details']) && !empty($mediaclip_obj['properties']['option_details'])) {
-				$option_details = json_decode($mediaclip_obj['properties']['option_details']);
-				$product_options = $product->getOptions();
-				foreach ($option_details as $oid => $ovalue) {
-					foreach ($product_options as $pvalue) {
-						 //print_r($pvalue->getData('option_id'));
-					 //print_r($pvalue);
-						if ($oid == $pvalue->getData('option_id')) {
-							$optionValues = $pvalue->getValues();
-							if ($optionValues) {
-
-								foreach ($optionValues as $_value) {
-									if ($ovalue == $_value->getData('option_type_id')) {
-										$custom_option_price = $custom_option_price + $_value->getPrice();
-									}
-								}
-							}
-						}
-					}
-				}
-			}
-			foreach ($mediaclip_obj['items'] as $key => $value) {
-				if (isset($value['photobookData']) && isset($value['photobookData']['additionalSheetCount'])) {
-					if ($value['photobookData']['additionalSheetCount'] > 0) {
-						$additionalPriceAmount = $this->getProductAdditionalPriceAmount($product_id);
-						$additional = $value['photobookData']['additionalSheetCount'];
-						$additionalPrice = $additionalPrice + ($additionalPriceAmount * (int)$additional);
-					}
-				}
-			}
-			if ($product->getMediaclipMinimumPrintsAllow()) {
-				$min_allow = $product->getMediaclipMinimumPrintsCount();
-				$add_price = $product->getMediaclipExtraPrintsPrice();
-				if ($min_allow != '' && is_numeric($min_allow) && $add_price != '' && is_numeric($add_price)) {
-					foreach ($mediaclip_obj['items'] as $mediaclipItem) {
-						if ($mediaclipItem['properties']['storeProductId'] == $product_id) {
-							$designer_prints = $mediaclipItem['quantity'];
-							if ($designer_prints > $min_allow) {
-								$diff = $designer_prints - $min_allow;
-								$additionalPrice = $diff*$add_price;
-							} else if ($designer_prints < $min_allow) {
-								$diff = $min_allow - $designer_prints;
-								$session = $this->_objectManager->get('Magento\Customer\Model\Session');
-								$session->setCanAddMoreMediaclipPrintsPrompt(1);
-								$add_more_prompt['diff'] = $diff;
-								$add_more_prompt['project_id'] = $mediaclip_obj['projectId'];
-								$add_more_prompt['product_id'] = $product_id;
-								$session->setAddMoreMediaclipPrints($add_more_prompt);
-							}
-						}
-					}
-				}
-			}
-			$price = $product_price + $custom_option_price + $additionalPrice;
-			$price = $price*$quantity;
-		}
-		return $price;
-	}
-
-	/**
-	 * 2019-05-14
-	 * @used-by getProductCalculatedPrice()
-	 * @param $productId
-	 * @return int
-	 */
-	private function getProductAdditionalPriceAmount($productId){
-		$product = $this->_objectManager->create('\Magento\Catalog\Model\Product')->load($productId);
-		if ($product->getMediaClipExtrasheetamt() && is_numeric($product->getMediaClipExtrasheetamt())) {
-			$additionalAmount = $product->getMediaClipExtrasheetamt();
-			return $additionalAmount;
-		}
-		return 0;
 	}
 
 	function getPriceHtml($productPrice){
